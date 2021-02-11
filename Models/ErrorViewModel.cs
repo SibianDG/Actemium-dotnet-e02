90ab5247ed321337ad52e7fdac3f76e4f20ab3d1
SibianDG <sibian.degussem@student.hogent.be>
2021-02-11 15:56:08 +0100

    Initialize Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2021_dotnet_e_02.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
