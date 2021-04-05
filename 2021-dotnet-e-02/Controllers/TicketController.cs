5ce02b80b8d67ddeadf39773cf71f192ebc7d8b4
SDG <sibiandegussem@outlook.com>
2021-04-05 11:53:01 +0200

    Scaffold TicketController & view

---
+++ 2021-dotnet-e-02/Controllers/TicketController.cs
@@ -0,0 +1,27 @@
+﻿using _2021_dotnet_e_02.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2021_dotnet_e_02.Controllers
+{
+    public class TicketController : Controller
+    {
+        private readonly ITicketRepository _ticketRepository;
+
+        public TicketController(ITicketRepository ticketRepository)
+        {
+            _ticketRepository = ticketRepository;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ActemiumTicket> tickets;
+            tickets = _ticketRepository.GetAll();
+            tickets = tickets.OrderBy(t => t.Priority).ThenBy(t => t.DateAndTimeOfCreation).ToList();
+            return View(tickets);
+        }
+    }
+}
