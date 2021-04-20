97728faf32ae9a09058a65e5c5e68ca1dc868f20
Florian Goossens <florian.goossens@student.hogent.be>
2021-04-20 15:10:07 +0200

    implemented Index() in HomeController (made the static html dynamic)

--- 2021-dotnet-e-02/Controllers/HomeController.cs
+++ 2021-dotnet-e-02/Controllers/HomeController.cs
@@ -6,21 +6,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using _2021_dotnet_e_02.Models;
+using _2021_dotnet_e_02.Models.Enums;
 
 namespace _2021_dotnet_e_02.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ITicketRepository _ticketRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ITicketRepository ticketRepository)
         {
             _logger = logger;
+            _ticketRepository = ticketRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<ActemiumTicket> allTickets = _ticketRepository.GetAll();
+            IEnumerable<ActemiumTicket> openTickets = _ticketRepository.GetAll().Where(t => t.Status != TicketStatus.COMPLETED);
+            IEnumerable<ActemiumTicket> resolvedTickets = _ticketRepository.GetAll().Where(t => t.Status == TicketStatus.COMPLETED);
+
+            ViewData["OpentTickets"] = openTickets.ToList().Count();
+            ViewData["ResolvedTickets"] = resolvedTickets.ToList().Count();
+
+            return View(allTickets);
         }
 
         public IActionResult Privacy()
