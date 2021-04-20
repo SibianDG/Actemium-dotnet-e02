7ac9054703133b3cbb6d8f62929c37fe62c952d2
IsBash <isaac.bauters@student.hogent.be>
2021-04-20 11:46:02 +0200

    Added some tests for TicketController

---
+++ 2021-dotnet-e-02.Tests/Controllers/TicketControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using _2021_dotnet_e_02.Controllers;
+using _2021_dotnet_e_02.Models;
+using _2021_dotnet_e_02.Tests.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using Xunit;
+
+namespace _2021_dotnet_e_02.Tests.Controllers
+{
+    public class TicketControllerTest
+    {
+        private readonly TicketController _controller;
+        private readonly DummyApplicationDbContext _dummyContext;
+        private readonly Mock<ITicketRepository> _ticketRepository;
+
+        public TicketControllerTest()
+        {
+            _dummyContext = new DummyApplicationDbContext();
+            _ticketRepository = new Mock<ITicketRepository>();
+            _controller = new TicketController(_ticketRepository.Object)
+            {
+                TempData = new Mock<ITempDataDictionary>().Object
+            };
+        }
+
+        #region -- Index --
+
+        [Fact]
+        public void Index_PassesListOfTicketsOrderedByPrioInViewResult()
+        {
+            _ticketRepository.Setup(t => t.GetAll()).Returns(_dummyContext.Tickets);
+            var result = Assert.IsType<ViewResult>(_controller.Index());
+            var ticketsInModel = Assert.IsType<List<ActemiumTicket>>(result.Model);
+            Assert.Equal(5, ticketsInModel.Count);
+            Assert.Equal("Title", ticketsInModel[0].Title);
+            Assert.Equal("Title4", ticketsInModel[1].Title);
+            Assert.Equal("Title2", ticketsInModel[2].Title);
+            Assert.Equal("Title5", ticketsInModel[3].Title);
+            Assert.Equal("Title3", ticketsInModel[4].Title);
+        }
+
+        #endregion
+    }
+}
---
+++ 2021-dotnet-e-02.Tests/Data/DummyApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using _2021_dotnet_e_02.Models;
+using _2021_dotnet_e_02.Models.Enums;
+
+namespace _2021_dotnet_e_02.Tests.Data
+{
+    public class DummyApplicationDbContext
+    {
+        public IEnumerable<ActemiumTicket> Tickets { get; }
+        public ActemiumTicket Ticket1 { get; } 
+        public ActemiumTicket Ticket2 { get; }
+        public ActemiumTicket Ticket3 { get; }
+        public ActemiumTicket Ticket4 { get; }
+        public ActemiumTicket Ticket5 { get; }
+        
+        public ActemiumCompany Google { get; }
+        public ActemiumCompany Amazon { get; }
+        
+        public ActemiumEmployee Admin { get; }
+        public ActemiumEmployee Tech { get; }
+        public ActemiumEmployee Supp { get; }
+        
+        public ActemiumCustomer Cust1 { get; }
+        public ActemiumCustomer Cust2 { get; }
+
+        public DummyApplicationDbContext()
+        {
+            int ticketId = 1;
+            
+            //TicketStatus status, TicketPriority priority, string title, string description, string attachments, TicketType type, string solution, string quality, string supportNeeded
+            Ticket1 = new ActemiumTicket(TicketStatus.CREATED, TicketPriority.P1, "Title", "Description", "Attachments", TicketType.HARDWARE, "Solution", "Quality", "Support needed");
+            Ticket2 = new ActemiumTicket(TicketStatus.IN_PROGRESS, TicketPriority.P2, "Title2", "Description2", "Attachments2", TicketType.NETWORK, "Solution2", "Quality2", "Support needed2");
+            Ticket3 = new ActemiumTicket(TicketStatus.IN_DEVELOPMENT, TicketPriority.P3, "Title3", "Description3", "Attachments3", TicketType.DATABASE, "Solution3", "Quality3", "Support needed3");
+            Ticket4 = new ActemiumTicket(TicketStatus.CANCELLED, TicketPriority.P1, "Title4", "Description4", "Attachments4", TicketType.SOFTWARE, "Solution4", "Quality4", "Support needed4");
+            Ticket5 = new ActemiumTicket(TicketStatus.COMPLETED, TicketPriority.P2, "Title5", "Description5", "Attachments5", TicketType.INFRASTRUCTURE, "Solution5", "Quality5", "Support needed5");
+
+            Tickets = new[] {Ticket1, Ticket2, Ticket3, Ticket4, Ticket5};
+        }
+        
+    }
+}
