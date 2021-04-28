d4b51d567762c209532314f232fbc60e49fe71bd
IsBash <isaac.bauters@student.hogent.be>
2021-04-28 16:57:58 +0200

    store enum as string in Json for contract

--- 2021-dotnet-e-02/Models/Domain/ActemiumContractType.cs
+++ 2021-dotnet-e-02/Models/Domain/ActemiumContractType.cs
@@ -1,4 +1,6 @@
 ﻿using _2021_dotnet_e_02.Models.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace _2021_dotnet_e_02.Models
 {
@@ -7,10 +9,12 @@ namespace _2021_dotnet_e_02.Models
         
         public int ContractTypeId { get; set; }
         public string Name { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
         public ContractTypeStatus Status { get; set; }
         public bool HasEmail { get; set; }
         public bool HasPhone { get; set; }
         public bool HasApplication { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
         public Timestamp TimeStamp { get; set; }
         public int MaxHandlingTime { get; set; }
         public int MinThroughputTime { get; set; }
