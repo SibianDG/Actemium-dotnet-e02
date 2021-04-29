75fa10f74a7adb9a562b4c5c8ab0a9c21e917890
SDG <sibiandegussem@outlook.com>
2021-04-29 13:05:07 +0200

    Layout changeswith feedback 2904

--- 2021-dotnet-e-02/Models/Domain/Enums/TicketPriority.cs
+++ 2021-dotnet-e-02/Models/Domain/Enums/TicketPriority.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace _2021_dotnet_e_02.Models.Enums
 {
@@ -10,19 +11,22 @@ namespace _2021_dotnet_e_02.Models.Enums
         // Standard notation for all companies
         // Priority and maximum handling time of a ticket is based on its type
         // P1: production impacted: solution needed within 2 hours
-		[EnumMember(Value = "P1")] 
+		[EnumMember(Value = "P1")]
+        [Display(Name = "High")]
         P1,
         /**
 	 * The P 2.
 	 */
         // P2: production will halt within due time: solution needed within 4 hours
         [EnumMember(Value = "P2")] 
+        [Display(Name = "Medium")]
         P2,
         /**
 	 * The P 3.
 	 */
         // P3: no impact on production: solution needed within 3 days
         [EnumMember(Value = "P3")] 
+        [Display(Name = "Low")]
         P3
     }
 }
