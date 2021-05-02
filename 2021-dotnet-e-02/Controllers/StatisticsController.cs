d5943281d6fa68930b3c895caf7b13f689111b7f
SDG <sibiandegussem@outlook.com>
2021-05-02 15:07:02 +0200

    Added empty Statistics Controller & Index view

---
+++ 2021-dotnet-e-02/Controllers/StatisticsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2021_dotnet_e_02.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
